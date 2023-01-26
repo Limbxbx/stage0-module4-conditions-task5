af529798a68e21b251c9ea54bc810685eb08546e
Илья <ilyich223@gmail.com>
2023-01-26 15:05:29 +0200

    Task 5 complete

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.conditions.task5;
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
         if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
-            System.out.println(year + "leap");
+            System.out.println("leap");
         } else {
-            System.out.println(year + "not leap");
+            System.out.println("not leap");
         }
     }
 }
